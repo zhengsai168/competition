6698a71b15720c1ed58606b9e2aa376a8a7a72a8
zhengsai5 <zhengsai3@jd.com>
2019-03-19 16:19:01 +0800

    xgboost param，feature，one-hot

--- 1_xgboost.py
+++ 1_xgboost.py
@@ -31,93 +31,98 @@ test = pd.get_dummies(test, columns=['weather'])
 # print(train.head())
 y_train = train.pop('y')
 
-param_grid = {
-    #'subsample': np.linspace(0.3, 1, 5),
-    #'colsample_bytree': np.arange(0.3,1,5)
-    #'min_child_weight':np.arange(1,16)
-    #'reg_alpha':[i*0.0 for i in range(10)]
-    #'gamma':np.linspace(0,0.2,30)
-    #'learning_rate':np.linspace(0.05,0.2,10)
-    #'n_estimators': [i*100 for i in np.arange(1,11)]
-    'subsample': np.linspace(0.8,1,20)
-    #'colsample_bytree': [i*0.05 for i in np.arange(16,21)]
-}
 
-random_param_dict = {
-    # 'max_depth': [3,4,5,6],
-    # 'learning_rate': np.linspace(0.02,0.2,10),
-    'n_estimators': range(100,1001,100)
-    # 'gamma': np.linspace(0.03,0.06,60),
-    # 'min_child_weight': np.arange(5,15),
-    # 'max_delta_step': np.arange(0,10),
-    # 'sub_sample': np.linspace(0.7,1,10),
-    # 'colsample_bytree': np.linspace(0.7,1,10),
-    # 'colsample_bylevel': np.linspace(0.7,0.75,30),
-    # 'reg_alpha': np.linspace(0.9,1.1,30),
-    # 'reg_lambda': np.linspace(0.1,1,10),
-    # 'scale_pos_weight': np.linspace(0.1,10,10)
-        # max_depth=3, learning_rate=0.1, n_estimators=100,
-        #          silent=True, objective="reg:linear",
-        #          nthread=-1, gamma=0, min_child_weight=1, max_delta_step=0,
-        #          subsample=1, colsample_bytree=1, colsample_bylevel=1,
-        #          reg_alpha=0, reg_lambda=1, scale_pos_weight=1
-}
-
-
-
-# xgbr = XGBRegressor(max_depth = 5,learning_rate=0.09,n_estimators=200,
-#                     reg_alpha=0.05,gamma=0.041379310344827593)
-                    #,subsample=0.8,colsample_bytree=0.8)
-                    #,min_child_weight=10,
-                    #subsample=0.7,colsample_bytree=0.3,reg_alpha=0.001,gamma=0.07)
-# xgbr = XGBRegressor(max_depth=5,n_estimators=200,learning_rate=0.086610169491525418,
-#                     reg_alpha=0.96896551724137936,gamma=0.03,
-#                     subsample=0.85, colsample_bylevel=0.71551724137931028,)
-rf_param_dict = {
-    # 'n_estimators': range(100,1001,100),
-    #'max_depth': range(3,15),
-    # 'min_samples_split': range(10,101,10),
-    # 'min_samples_leaf': range(1,11),
-    # 'max_features': range(2,8)
-}
 #
-# rf = RandomForestRegressor(n_estimators=800, max_depth=12,min_samples_split=10,min_samples_leaf=2,
-#                            max_features=6, )
-# # ,max_depth=5,min_child_weight=15,gamma=0.2,subsample=0.75)
-# xgbr = XGBRegressor()
-# grid = GridSearchCV(xgbr,random_param_dict,scoring='neg_mean_squared_error')
-# grid = RandomizedSearchCV(xgbr,random_param_dict,n_iter=10,scoring='neg_mean_squared_error')
-x = []
-y = []
-for i in range(100,1001,100):
-    # xgbr = XGBRegressor(n_estimators=i, learning_rate=0.08, max_depth=4,min_child_weight=16,subsample=1)
-    xgbr = XGBRegressor(max_depth=5,n_estimators=i,learning_rate=0.086610169491525418,
-                      reg_alpha=0.96896551724137936,gamma=0.03,
-                      subsample=0.85, colsample_bylevel=0.71551724137931028)
-    grid = GridSearchCV(xgbr,rf_param_dict,scoring='neg_mean_squared_error')
-    grid.fit(train, y_train)
-    x.append(i)
-    y.append(grid.best_score_)
-    #print(grid.best_params_)
-    #print(grid.best_score_)
+rf = RandomForestRegressor(n_estimators=800, max_depth=12,min_samples_split=10,min_samples_leaf=2,
+                           max_features=6, )
+# ,max_depth=5,min_child_weight=15,gamma=0.2,subsample=0.75)
+xgbr = XGBRegressor(max_depth = 5,learning_rate=0.09,n_estimators=200,
+                    reg_alpha=0.05,gamma=0.041379310344827593)
 
-plt.plot(x,y)
-plt.show()
 
+xgbr.fit(train, y_train)
+rf.fit(train, y_train)
+
+y_pred_1 = xgbr.predict(test)
+y_pred_1 = list(map(lambda x: x if x >= 0 else 0, y_pred_1))
+
+y_pred_2 = rf.predict(test)
+y_pred_2 = list(map(lambda x: x if x >= 0 else 0, y_pred_2))
+
+y_pred = [(t1+t2)/2.0 for t1,t2 in zip(y_pred_1,y_pred_2)]
+
+submit['y'] = y_pred_1
+submit.to_csv('my_xgb_prediction_7.csv', index=False)
+
+
+# x = []
+# y = []
+# for i in range(100,1001,100):
+#     # xgbr = XGBRegressor(n_estimators=i, learning_rate=0.08, max_depth=4,min_child_weight=16,subsample=1)
+#     xgbr = XGBRegressor(max_depth=5,n_estimators=i,learning_rate=0.086610169491525418,
+#                       reg_alpha=0.96896551724137936,gamma=0.03,
+#                       subsample=0.85, colsample_bylevel=0.71551724137931028)
+#     grid = GridSearchCV(xgbr,rf_param_dict,scoring='neg_mean_squared_error')
+#     grid.fit(train, y_train)
+#     x.append(i)
+#     y.append(grid.best_score_)
+#     #print(grid.best_params_)
+#     #print(grid.best_score_)
+#
 # plt.plot(x,y)
 # plt.show()
 
-# grid.fit(train, y_train)
-# # rf.fit(train, y_train)
+# param_grid = {
+#     #'subsample': np.linspace(0.3, 1, 5),
+#     #'colsample_bytree': np.arange(0.3,1,5)
+#     #'min_child_weight':np.arange(1,16)
+#     #'reg_alpha':[i*0.0 for i in range(10)]
+#     #'gamma':np.linspace(0,0.2,30)
+#     #'learning_rate':np.linspace(0.05,0.2,10)
+#     #'n_estimators': [i*100 for i in np.arange(1,11)]
+#     'subsample': np.linspace(0.8,1,20)
+#     #'colsample_bytree': [i*0.05 for i in np.arange(16,21)]
+# }
+#
+# random_param_dict = {
+#     # 'max_depth': [3,4,5,6],
+#     # 'learning_rate': np.linspace(0.02,0.2,10),
+#     'n_estimators': range(100,1001,100)
+#     # 'gamma': np.linspace(0.03,0.06,60),
+#     # 'min_child_weight': np.arange(5,15),
+#     # 'max_delta_step': np.arange(0,10),
+#     # 'sub_sample': np.linspace(0.7,1,10),
+#     # 'colsample_bytree': np.linspace(0.7,1,10),
+#     # 'colsample_bylevel': np.linspace(0.7,0.75,30),
+#     # 'reg_alpha': np.linspace(0.9,1.1,30),
+#     # 'reg_lambda': np.linspace(0.1,1,10),
+#     # 'scale_pos_weight': np.linspace(0.1,10,10)
+#         # max_depth=3, learning_rate=0.1, n_estimators=100,
+#         #          silent=True, objective="reg:linear",
+#         #          nthread=-1, gamma=0, min_child_weight=1, max_delta_step=0,
+#         #          subsample=1, colsample_bytree=1, colsample_bylevel=1,
+#         #          reg_alpha=0, reg_lambda=1, scale_pos_weight=1
+# }
+
+#
+#
+# # xgbr = XGBRegressor(max_depth = 5,learning_rate=0.09,n_estimators=200,
+# #                     reg_alpha=0.05,gamma=0.041379310344827593)
+#                     #,subsample=0.8,colsample_bytree=0.8)
+#                     #,min_child_weight=10,
+#                     #subsample=0.7,colsample_bytree=0.3,reg_alpha=0.001,gamma=0.07)
+# # xgbr = XGBRegressor(max_depth=5,n_estimators=200,learning_rate=0.086610169491525418,
+# #                     reg_alpha=0.96896551724137936,gamma=0.03,
+# #                     subsample=0.85, colsample_bylevel=0.71551724137931028,)
+# rf_param_dict = {
+#     # 'n_estimators': range(100,1001,100),
+#     #'max_depth': range(3,15),
+#     # 'min_samples_split': range(10,101,10),
+#     # 'min_samples_leaf': range(1,11),
+#     # 'max_features': range(2,8)
+# }
+
+# grid = GridSearchCV(xgbr,random_param_dict,scoring='neg_mean_squared_error')
+# grid = RandomizedSearchCV(xgbr,random_param_dict,n_iter=10,scoring='neg_mean_squared_error')
 # print(grid.best_params_)
 # print(grid.best_score_)
-# y_pred_1 = grid.predict(test)
-# y_pred_1 = list(map(lambda x: x if x >= 0 else 0, y_pred_1))
-
-# y_pred_2 = rf.predict(test)
-# y_pred_2 = list(map(lambda x: x if x >= 0 else 0, y_pred_2))
-
-# y_pred = [(t1+t2)/2.0 for t1,t2 in zip(y_pred_1,y_pred_2)]
-#
-# submit['y'] = y_pred_1
-# submit.to_csv('my_xgb_prediction_6.csv', index=False)
